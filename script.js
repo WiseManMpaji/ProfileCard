2cdfc2474aa2f2ef94b2997e4b28657839c0e61d
WiseManMpaji <130596688+WiseManMpaji@users.noreply.github.com>
2023-07-12 15:42:09 +0300

    updated

--- script.js
+++ script.js
@@ -2,3 +2,4 @@ function toggleMenu() {
     var menuContent = document.getElementById("menu-content");
     menuContent.style.display = menuContent.style.display === "block" ? "none" : "block";
 }
+

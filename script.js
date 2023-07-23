b8f0314ca6c25491c022b26108ff760a59ef6f1f
WiseManMpaji <130596688+WiseManMpaji@users.noreply.github.com>
2023-07-23 12:57:24 +0300

    updated

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ function toggleMenu() {
     menuContent.style.display = menuContent.style.display === "block" ? "none" : "block";
 }
 
+document.querySelector('#contact-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    e.target.elements.name.value = '';
+    e.target.elements.email.value = '';
+    e.target.elements.message.value = '';
+  });
